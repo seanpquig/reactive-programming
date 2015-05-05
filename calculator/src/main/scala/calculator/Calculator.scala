2dae71820e4fd2010523c2776ea2de41149e96ee
squigley <squigley@shutterstock.com>
2015-05-05 01:49:16 -0400

    completed more of calculator assignment

--- calculator/src/main/scala/calculator/Calculator.scala
+++ calculator/src/main/scala/calculator/Calculator.scala
@@ -11,11 +11,19 @@ final case class Divide(a: Expr, b: Expr) extends Expr
 object Calculator {
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+
+    namedExpressions.map{ case (s, expr) => (s, Signal(eval(expr(), namedExpressions)))}
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+    expr match {
+      case Literal(d)   => d
+      case Ref(s)       => eval(getReferenceExpr(s, references), references)
+      case Plus(a, b)   => eval(a, references) + eval(b, references)
+      case Minus(a, b)  => eval(a, references) - eval(b, references)
+      case Times(a, b)  => eval(a, references) * eval(b, references)
+      case Divide(a, b) => eval(a, references) / eval(b, references)
+    }
   }
 
   /** Get the Expr for a referenced variables.
--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,11 +3,28 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+
+    val delta: Double = b() * b() - 4 * a() * c()
+    Signal(delta)
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+
+    val solutionSet: Set[Double] = Set()
+    val delta = computeDelta(a, b, c)
+
+    if (delta() < 0)
+      Signal(solutionSet)
+    else if (delta() == 0) {
+      val root = -b() / (2.0 * a())
+      Signal(solutionSet + root)
+    }
+    else {
+      val root1 = (-b() + scala.math.sqrt(delta())) / (2 * a())
+      val root2 = (-b() - scala.math.sqrt(delta())) / (2 * a())
+      Signal(solutionSet + root1 + root2)
+    }
+
   }
 }
